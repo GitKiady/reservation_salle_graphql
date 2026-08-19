e6ed23cf2c72160960670f273c93ce0d2c9b43f5
kd <andriampianinana@gmail.com>
2026-08-19 20:40:49 +0300

    Add planning task

--- utils/planning/taskiq.py
+++ utils/planning/taskiq.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 
-from taskiq_redis import RedisAsyncResultBackend, RedisStreamBroker
+from taskiq_redis import RedisAsyncResultBackend, RedisStreamBroker, ListRedisScheduleSource
+from taskiq import TaskiqScheduler
 
 from utils.cache.redis_config import Redis
 
@@ -15,8 +16,21 @@ broker = RedisStreamBroker(
     url=redis.get_url(),
 ).with_result_backend(result_backend)
 
+# C'est celui qui enqueue le job
+schedule_source = ListRedisScheduleSource(url=f"{redis.get_url()}/1") # Différent database
+
+# Appeler par la commande: taskiq scheduler utils.planning.taskiq:scheduler
+scheduler = TaskiqScheduler(broker, sources=[schedule_source]) 
+
 
 @broker.task
 async def send_email_confirmation(room_id: int, start: datetime):
     # Envoie email 30 mins avant start date
+    ...
+
+@broker.task
+async def cancel_reservation(room_id: int):
+    # vérifie si booking n'a pas été confirmé mais toujours en pending
+    # si non, return none
+    # Modifie booking en cancel et supprimer la clé dans redis
     ...
